64f32a3ba8bd53491d5b93d1c21150a042748c95
SOLOMON ISRAEL <75482038+Israel-2001@users.noreply.github.com>
2023-09-09 06:22:58 +0100

    Hng task 1

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Create a new Date object to get the current date and time in the user's local timezone
+const localDate = new Date();
+
+function formatDate(dayOfWeek) {
+  var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  return daysOfWeek[dayOfWeek];
+}
+
+const currentDay = formatDate(localDate.getUTCDay());
+
+var utcDay = document.getElementById("day").textContent = currentDay
+
+console.log(currentDay);
+
+// Get the UTC time in milliseconds by subtracting the timezone offset
+const utcMilliseconds = localDate.getTime() - (localDate.getTimezoneOffset() * 60000);
+
+var currentUTCTime = document.getElementById("currentUTCTime").textContent = utcMilliseconds;
+
+console.log(currentUTCTime);
+
+// Output the UTC time in milliseconds
+console.log(utcMilliseconds);
